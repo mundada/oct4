4dd595928dc0361e5f4e4fd4bd7bf7de3254e27d
ashish mundada <amundada04@gmail.com>
2017-10-23 19:33:17 -0500

    first commit

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,46 +1,45 @@
 import React, { Component } from 'react';
 import './App.css';
-import Ashish from './Ashish.js';
-import Tick from './Tick.js';
+
 import logo from './AM.jpg';
-import logo1 from './buffalo.png';
-import Clock from './State.js';
-import Toggle from './Eventhandler.js';
-import ConditionalHeader from './ConditionalHeader';
-import Listandkey from './Listandkey';
-import TemperatureInput from './Liftingstate';
-import BasicExample from './ReactDom';
-/*import FancyBorder from './Composition';*/
+import logo1 from './buffalo.png'
+import img from './download.jpg';
+import img1 from './w.jpg';
+import img2 from './w1.jpg';
+import img3 from './w2.jpg';
+import c2 from './c2.jpg';
+
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-           <h1 className="App-title">Where Dreams Come True</h1>
+         
+          <img style = { { height: '150px', width: '1000px'}}src={c2} className="App-header" alt="header" />
+           <h1 className="App-title">Nobody Beats Our Prices, Guaranteed!</h1>
+           
         </header>
   
-        <p className="App-intro">
+       <p className="App-intro">
           Website under construction.... <code>Will be coming soon!</code>
         </p>
-      <ConditionalHeader/>
-        <Listandkey/>
-        <Tick/>
-        <Ashish/>
-        <Clock />
-        <Toggle/>
-        <TemperatureInput/>
-       
-       
-        <BasicExample/>
-        <footer classname= "App-footer">
+     
+        <div class="div1">This div is smaller (width is 300px and height is 100px).</div>
+       <h1><b> FIND THE RIGHT WASHERS FOR YOU </b></h1>
+       <h3> SEE ALL WASHERS > </h3>
+        <div class="flex-container">
+       <div> <img src={img} className="App-img" alt="img" /></div>
+        <div> <img src={img1} className="App-img1" alt="img1"/></div>
+        <div> <img src={img2} className="App-img2" alt="img2"/></div>
+        <div> <img src={img3} className="App-img3" alt="img3"/></div>
+</div>
+       <footer classname= "App-footer">
         <img src={logo1} className="App-logo1" alt="logo1" />
         <h3 className="App-title">Thank You For Visiting this Page</h3>
         </footer>
-      </div>
+  </div>
+  
     );
   }
 } 
-
-
 export default App;
